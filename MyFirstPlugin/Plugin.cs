77bfbb718925e5412dc4254af4549b035a36a55e
George Pied <george.pied@gmail.com>
2025-01-18 15:45:58 +0100

    Working perfectly, and configurable. Still need to smooth out the lighting.

--- MyFirstPlugin/Plugin.cs
+++ MyFirstPlugin/Plugin.cs
@@ -9,45 +9,36 @@ namespace MyFirstPlugin;
 [BepInPlugin(MyPluginInfo.PLUGIN_GUID, MyPluginInfo.PLUGIN_NAME, MyPluginInfo.PLUGIN_VERSION)]
 public class Plugin : BaseUnityPlugin
 {
-    private ConfigEntry<string> configGreeting;
-    private ConfigEntry<bool> configDisplayGreeting;
+    public static ConfigEntry<int> configMultiplier;
 
     internal static new ManualLogSource Logger;
         
     private void Awake()
     {
-        configGreeting = Config.Bind("General",      // The section under which the option is shown
-            "GreetingText",  // The key of the configuration option in the configuration file
-            "Hello, world!", // The default value
-            "A greeting text to show when the game is launched"); // Description of the option to show in the config file
-
-        configDisplayGreeting = Config.Bind("General.Toggles", 
-            "DisplayGreeting",
-            true,
-            "Whether or not to show the greeting text");
+        configMultiplier = Config.Bind("General",      // The section under which the option is shown
+            "TimeMultiplier",  // The key of the configuration option in the configuration file
+            8, // The default value
+            "Time multiplier"); // Description of the option to show in the config file
         // Plugin startup logic
         var harmony = new Harmony("george.no_time");
         harmony.PatchAll();
         Logger = base.Logger;
         Logger.LogInfo($"Plugin {MyPluginInfo.PLUGIN_GUID} is loaded! 420 blaze it");
-        if(configDisplayGreeting.Value)
-            Logger.LogInfo(configGreeting.Value);
     }
+
 }
 
 [HarmonyPatch(typeof(LevelInfo), "Update")]
-class Patch
+class TimePatch
 {
-    static bool Prefix(LevelInfo __instance)
+    static void Postfix(LevelInfo __instance)
     {   
-        __instance.NetworktimeOfDay = __instance.timeOfDay + Time.deltaTime * 0.00027777778f*59;
+        float mult = Plugin.configMultiplier.Value;
+        __instance.NetworktimeOfDay = __instance.timeOfDay + Time.deltaTime * 0.00027777778f * (mult-1f);
         if (__instance.timeOfDay > 24f)
-		{
-			__instance.NetworktimeOfDay = __instance.timeOfDay - 24f;
-		}
-        float time_of = __instance.timeOfDay;
-        string a = "Current time" + time_of.ToString();
-        Plugin.Logger.LogInfo(a);
-        return true;
+        {
+            __instance.NetworktimeOfDay = __instance.timeOfDay - 24f;
+        }
+        return;
     }
 }
