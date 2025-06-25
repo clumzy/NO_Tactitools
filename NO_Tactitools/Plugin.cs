a854b23b4d196647ccf73adea070218cb7a7b173
George Pied <george.pied@gmail.com>
2025-06-25 18:53:33 +0200

    Update version number to 0.1.01 in project files and plugin metadata

--- NO_Tactitools/Plugin.cs
+++ NO_Tactitools/Plugin.cs
@@ -7,7 +7,7 @@ using UnityEngine;
 using System.Collections.Generic;
 
 namespace NO_Tactitools {
-    [BepInPlugin("NO_Tactitools", "NOTT", "0.4.20")]
+    [BepInPlugin("NO_Tactitools", "NOTT", "0.1.01")]
     public class Plugin : BaseUnityPlugin {
         public static Harmony harmony;
         public static ConfigEntry<bool> targetRecallEnabled;
